679d0d68c1a93d3af3dff1506a404eac54f7e688
mousumi-ish <baral_mousumi@yahoo.com>
2023-09-13 11:50:52 +0100

    feat: implemented router

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,15 @@
-export {}
+import { Outlet } from "react-router";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+
+const MainLayout = () => (
+  <>
+    <Header />
+    <main className="p-6" style={{ flex: "1" }}>
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
+
+export default MainLayout;
--- src/components/products/product/product.tsx
+++ src/components/products/product/product.tsx
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
 
 const Product = () => {
-   
-return(<section className='content has-text-centered'>
-    <p>Welcome to the page for product:</p>
-    <p><strong className='is-size-1'> ?? </strong></p>
-</section>)}
+  const params = useParams();
+  const { productId } = params; // destructure the params object
+
+  return (
+    <section className="content has-text-centered">
+      <p>Welcome to the page for product:</p>
+      <p>
+        <strong className="is-size-1"> {productId} </strong>
+      </p>
+    </section>
+  );
+};
 
 export default Product;
